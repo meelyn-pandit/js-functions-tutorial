3afcafda30cebda1055e277a5b3fbed1a1ecf294
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-08-10 10:27:42 -0400

    initial commit

---
+++ tutorial.js
@@ -0,0 +1,5 @@
+/**What are and Why JS Functions */
+// avoids repeating the same tasks again and again
+// let's you run lines of code multiple times rather than copying code over and over
+// reduce lines of code to run
+
